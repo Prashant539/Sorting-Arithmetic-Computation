7a86b067852c7636b40a0541680252a6f26c01b5
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-11-20 23:29:37 +0530

    [Prashant]- Dictionary to Array

--- SortingArithmaticProblem.sh
+++ SortingArithmaticProblem.sh
@@ -18,6 +18,15 @@ expression[exp2]=$x;
 expression[exp3]=$y;
 expression[exp4]=$z;
 echo ${expression[@]}
+declare -A expression
+expression[exp1]=$w;
+expression[exp2]=$x;
+expression[exp3]=$y;
+expression[exp4]=$z;
+	echo ${expression[exp1]}
+	echo ${expression[exp2]}
+	echo ${expression[exp3]}
+	echo ${expression[exp4]}
 
 
 
